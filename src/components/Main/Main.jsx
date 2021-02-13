f9f88c373e1b6d321e2f75dcf075c109991192e4
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-02-13 23:39:03 +0530

    List component in main is completed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Card,CardHeader,CardContent,Typography,Grid,Divider} from '@material-ui/core'
 
 import Form from './Form/Form'
+import List from './List/List'
 import useStyles from './styles'
 
 const Main = () => {
@@ -23,7 +24,7 @@ const Main = () => {
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        {/* <List></List> */}
+                       <List />
                     </Grid>
                 </Grid>
             </CardContent>
