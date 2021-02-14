88fcbfd628c6d03a8fa15d4a540ca69d1870b4ec
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-02-14 14:53:46 +0530

    contextReducer function added

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import {ExpenseTrackerContext} from '../../../context/context'
 import {List as MUIList,ListItem,ListItemAvatar,ListItemText,Avatar,ListItemSecondaryAction,IconButton,Slide} from '@material-ui/core'
 import {Delete , MoneyOff} from '@material-ui/icons'
 import useStyles from './styles'
 
 const List = () => {
+
+    const {deleteTransaction} =useContext(ExpenseTrackerContext)
+
     const classes = useStyles()
-    const transactions=[{id:1,type:"Income",category:"bussiness",amount:50,date:new Date().toLocaleString()}]
+    const transactions=[{id:1,type:"Income",category:"bussiness",amount:50,date:new Date().toLocaleString()},{id:2,type:"Expense",category:"pet",amount:50,date:new Date().toLocaleString()},{id:3,type:"Income",category:"salary",amount:50,date:new Date().toLocaleString()}]
 
     return (
         <MUIList dense={false} className={classes.list}>
