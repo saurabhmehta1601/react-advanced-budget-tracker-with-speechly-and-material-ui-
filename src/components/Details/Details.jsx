f27643bdedbdb575867dfcc06b1bc3bdeab73288
Saurabh Mehta <saurabhemehta1601@gmail.com>
2021-02-16 17:34:58 +0530

    useTransaction hook done Doughnut graph made

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Card,CardHeader,CardContent,Typography } from '@material-ui/core'
 import {Doughnut} from 'react-chartjs-2'
+import useTransactions from '../../useTransactions'
 
 import useStyles from './styles'
 
 const Details = ({title}) => {
+
     const classes=useStyles()
+    const {total,chartData} =useTransactions(title)
+
+    
+
     return (
-        <Card   className={title==="Income" ? classes.income : classes.expense}>
+        <Card  className={title==="Income" ? classes.income : classes.expense}>
             <CardHeader title={title} />
             <CardContent>
                 <Typography variant="h5"> 
-                    $50
+                 ${total}
                 </Typography>
-                {/* <Doughnut data="Data"/> */}
+                <Doughnut data= {chartData} />
+
+               
             </CardContent>
         </Card>
     )
---
+++ src/useTransactions.js
@@ -0,0 +1,43 @@
+import {useContext} from 'react'
+import  {ExpenseTrackerContext} from "./context/context"
+import {expenseCategories,incomeCategories,resetCategories} from './constants/categories'
+
+const useTransactions = (title) => {
+
+    resetCategories()
+
+    const {transactions} =useContext(ExpenseTrackerContext)
+    console.log(transactions);
+    const transactionsPerCategory = transactions.filter(t => t.type===title)
+
+   
+
+    const total = transactionsPerCategory.reduce(((acc,currVal)=> acc+=currVal.amount),0)
+
+    const categories=( title==="Income") ? incomeCategories: expenseCategories
+
+    transactionsPerCategory.forEach((t)=>{
+        const category= categories.find( c => c.type===t.category)
+
+        if(category) category.amount+=t.amount
+    })
+
+    
+
+    const filteredCategories =categories.filter((c) => c.amount > 0 )
+    
+    
+
+    const chartData={
+        datasets:[{
+            data: filteredCategories.map( c => c.amount ),
+            backgroundColor:  filteredCategories.map( c => c.color )
+        }],
+        labels:filteredCategories.map( c => c.type)
+    }
+
+
+    return {chartData,total}
+}
+
+export default useTransactions
